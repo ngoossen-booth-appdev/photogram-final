d6f7d7da8c23a860d5a0bc70a905d9389af2c1fe
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-11-24 19:48:18 +0000

    likes progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_194646) do
+ActiveRecord::Schema.define(version: 2020_11_24_194746) do
+
+  create_table "follow_requests", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
