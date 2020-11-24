17c6619825d9ef8f3e8ef5cb94d19ab8acd39f6d
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-11-24 19:48:54 +0000

    comments db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_194746) do
+ActiveRecord::Schema.define(version: 2020_11_24_194837) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "commenter_id"
+    t.integer "photo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "follow_requests", force: :cascade do |t|
     t.integer "sender_id"
